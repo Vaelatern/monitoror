2a5820a6abdd9a51e605a3707f6d54eb4b3d0bdc
Toyam Cox <Vaelatern@voidlinux.org>
2024-05-21 23:51:40 -0400

    what is with rice
    why are all these deps hidden

--- scripts/package/front.sh
+++ scripts/package/front.sh
@@ -7,5 +7,6 @@ set -e
 # This script is used to package ui/dist in go source #
 #######################################################
 
+go get github.com/GeertJohan/go.rice
 rm -f service/rice-box.go
 rice embed-go -i ./service
